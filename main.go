6ce2deb88f92801a6c5b576c6c945290cdf3be28
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-09-02 15:53:22 +0300

    wip

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+)
+
+type Backend struct {
+	URL          *url.URL
+	ReverseProxy *httputil.ReverseProxy
+}
+
+type LoadBalancer struct {
+	backends []*Backend
+	mutex    *sync.Mutex
+	next     int
+}
+
+func (b *LoadBalancer) AddBackend(backend *Backend) {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+	b.backends = append(b.backends, backend)
+}
 
 func main() {
 	fmt.Println("Let's create load balancer")
