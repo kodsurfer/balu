c987d4584f9026cf933537e56e6b5216b3a146f2
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-09-02 16:06:53 +0300

    Update main.go

--- main.go
+++ main.go
@@ -24,6 +24,19 @@ func (b *LoadBalancer) AddBackend(backend *Backend) {
 	b.backends = append(b.backends, backend)
 }
 
+func (b *LoadBalancer) NextBackend() *Backend {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+	if len(b.backends) == 0 {
+		return nil
+	}
+
+	res := b.backends[b.next]
+	b.next = (b.next + 1) % len(b.backends)
+
+	return res
+}
+
 func main() {
 	fmt.Println("Let's create load balancer")
 }
