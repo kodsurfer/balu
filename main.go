b4d8eed732c904b48e752f0218f213fc383d77fc
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-09-13 18:02:11 +0300

    Update main.go

--- main.go
+++ main.go
@@ -77,4 +77,18 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+	
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting")
 }
